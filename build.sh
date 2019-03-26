39325d450724063978f0cf35a6b1a2d52d2d4dcc
Marius Stein <mariusstein7@gmail.com>
2019-03-26 17:32:27 +0100

    optimized build script

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@ docker build . -t twitter-to-elastic
 commitSha=$(git rev-parse HEAD)
 docker tag twitter-to-elastic mariusstein77/twitter-to-elastic:$commitSha
 docker push mariusstein77/twitter-to-elastic:$commitSha
+
+echo $commitSha
