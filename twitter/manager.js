caa60693d388da5c8f7bdafc26f023d92cfce02e
Marius Stein <Marius.Stein@Viadee.de>
2020-02-15 21:53:24 +0100

    var -> const; function -> arrow functions

--- twitter/manager.js
+++ twitter/manager.js
@@ -1,20 +1,20 @@
-var twitterConfig = require('./twitterConfig.js');
-var Twit = require('twit');
+const twitterConfig = require("./twitterConfig.js");
+const Twit = require("twit");
 
-var manager = {
+const manager = {
     T: new Twit(twitterConfig.connectionConfig),
-    listenOnStream: function(filter, callback) {
+    listenOnStream: (filter, callback) => {
         if (filter == null) {
             filter = twitterConfig.streamConfig.query;
         }
-        var stream = this.T.stream('statuses/filter', {track: filter, tweet_mode: "extended" })
-        stream.on('tweet', function (tweet) {
+        const stream = this.T.stream("statuses/filter", { track: filter, tweet_mode: "extended" });
+        stream.on("tweet", tweet => {
             callback(tweet);
-        })
+        });
     },
-    get: function (path, params, callback) {
+    get: function(path, params, callback) {
         this.T.get(path, params, callback);
-    }
-}
+    },
+};
 
 module.exports = manager;
